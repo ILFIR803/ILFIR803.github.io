78719655a08acb857f5c9cf7573941efa8a53ade
Ilfir <ilfir803@gmail.com>
2023-10-07 20:58:09 +0500

    close menu click overlay

--- js/script.js
+++ js/script.js
@@ -1,15 +1,18 @@
 const humburger = document.querySelector('.humburger'),
       menu = document.querySelector('.menu'),
-      closeMenu = document.querySelector('.menu__close');
+      closeMenu1 = document.querySelector('.menu__close'),
+      closeMenu2 = document.querySelector('.menu__overlay');
 
 humburger.addEventListener('click', () => {
    menu.classList.add('active');
 });
 
-closeMenu.addEventListener('click', () => {
+closeMenu1.addEventListener('click', () => {
    menu.classList.remove('active');
 });
-
+closeMenu2.addEventListener('click', () => {
+   menu.classList.remove('active');
+});
 const counters = document.querySelectorAll('.skills__progress-percent'),
       lines = document.querySelectorAll('.skills__progress-scale');
 
